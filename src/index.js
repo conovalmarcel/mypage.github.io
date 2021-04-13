389e47e3ee62506b3b41272c53b034c8ecc96287
conovalmarcel <conovalmarcel@gmail.com>
2021-04-13 13:03:02 +0300

    add work links

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-jQuery(document).ready(function($) {});
+const question = prompt("Hello! Do you want play a game?");
+console.log(question);
+
+function checkansw(usersansw) {
+  console.log(usersansw);
+  if ((usersansw = yes)) {
+    console.log("Lets do it");
+  } else {
+    console.log("You are not interesting person");
+  }
+}
+
+checkansw(question);
